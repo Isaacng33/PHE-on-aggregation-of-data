ca14efab063430d0a3cff10aafaccf4f48ce5dcc
Isaac Ng <isaacnpump@gmail.com>
2024-10-18 15:29:10 +0100

    fully working encryption and decryption

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,7 +1,14 @@
 #tests/test_encryption.py
 '''Module to test encryption methods'''
 import os
-from app.encryption import generate_keys, KEY_DIR, load_context_public, load_secret_key
+from app.encryption import (
+    KEY_DIR,
+    generate_keys,
+    load_context_public,
+    load_secret,
+    encrypt_int,
+    decrypt_int
+)
 
 def test_generate_keys():
     '''Test for key'''
@@ -10,7 +17,7 @@ def test_generate_keys():
         os.remove(os.path.join(KEY_DIR, file))
 
     # Call the generate_keys function
-    result = generate_keys()
+    result = generate_keys(30)
 
     # Check if the function returned True
     assert result is True
@@ -30,8 +37,40 @@ def test_load_secret_key():
     encryption_obj = load_context_public()
     assert encryption_obj is not None
 
-    assert load_secret_key(encryption_obj) is True
+    assert load_secret(encryption_obj) is not None
 
+def test_encrypt_int():
+    '''Ensure encryption of an integer works'''
+    encryption_obj = load_context_public()
+    assert encryption_obj is not None
+
+    data = [10, -10, 0, 42, -42, 12345, -12345, 1000000, -1000000]
+    for integer in data:
+        ciphertext = encrypt_int(encryption_obj, integer)
+        assert ciphertext is not None, (F"Encryption failed for integer {integer}")
+        print(f"Test passed for encryption integer: {integer}, Encrypted data: {ciphertext}")
+    print("All test case passed for encryption")
+
+def test_decrypt_int():
+    '''Ensure decryption of an integer works'''
+    encryption_obj = load_context_public()
+    assert encryption_obj is not None
+
+    key = load_secret(encryption_obj)
+    assert key is not None
+
+    # Encryption data set
+    data = [10, -10, 0, 42, -42, 12345, -12345, 1000000, -1000000]
+    for integer in data:
+        ciphertext = encrypt_int(encryption_obj, integer)
+        assert ciphertext is not None
+
+        decrypted_int = decrypt_int(key, ciphertext)
+        if decrypted_int != integer:
+            print(f"Integer: {integer}, Decrypted integer: {decrypted_int}")
+        assert decrypted_int == integer, (f"Decryption failed for integer {integer}")
+        print(f"Test passed for decryption integer: {integer}, Decrypted data: {decrypted_int}")
+    print("All test case passed for decryption")
 
 def clear_keys():
     '''Clean up after the tests'''
